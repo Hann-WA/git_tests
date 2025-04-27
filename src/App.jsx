249722a3f61c625862dab60787a58cf6881a4b5e
Hann <hanwin.aung@kmutt.ac.th>
2025-04-27 16:56:24 +0700

    change Edit text to Footer

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           Counter No. {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Footer <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
